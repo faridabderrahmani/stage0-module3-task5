57dad1da8e5c7a3f1982fda11bbfc65ddef17b68
faridabderrahmani <abderrahmani@yandex.ru>
2023-12-28 19:35:25 +0100

    Resolved

--- src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
+++ src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
@@ -2,5 +2,13 @@ package lang.print.gaps.task5;
 
 public class ThreeStuckVars {
     public static void main(String[] args) {
+
+        int first, second, third;
+        first = 1;
+        second = 10;
+        third = 100;
+        System.out.print(first);
+        System.out.print(second);
+        System.out.print(third);
     }
 }
